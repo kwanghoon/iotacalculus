24334ad07abce0e15b186c6b25e37df4d5db057f
Kwanghoon Choi <lazyswamp@gmail.com>
2023-08-12 17:55:12 +0900

    update

--- app/Main.hs
+++ app/Main.hs
@@ -18,7 +18,10 @@ import TokenInterface
 import Data.Aeson
 import GHC.Generics
 
--- Pretty printing JSON
+------------------------------------------------------------------------
+-- | Pretty printing JSON
+------------------------------------------------------------------------
+
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as E
 import qualified Data.ByteString.Lazy as LBS
@@ -26,6 +29,17 @@ import Data.Aeson.Encode.Pretty (encodePretty)
 
 import qualified Data.Text.IO as DTIO
 
+------------------------------------------------------------------------
+-- | Pretty printing JSON
+------------------------------------------------------------------------
+
+lsbToText :: LBS.ByteString -> T.Text
+lsbToText = E.decodeUtf8 . LBS.toStrict
+
+jsonToText :: Rule -> T.Text
+jsonToText = lsbToText . encodePretty
+
+------------------------------------------------------------------------
 
 -- Main
 main :: IO ()
@@ -57,9 +71,3 @@ doProcess verbose fileName = do
   when (verbose) $ DTIO.putStrLn (jsonToText (fromASTRule astRule))
   when (verbose) $ putStrLn "Done."
 
--- Pretty printing JSON
-lsbToText :: LBS.ByteString -> T.Text
-lsbToText = E.decodeUtf8 . LBS.toStrict
-
-jsonToText :: Rule -> T.Text
-jsonToText = lsbToText . encodePretty
